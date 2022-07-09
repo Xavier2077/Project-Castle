dc0bdcae3052c6863ecbddf9710e1c30ce725d24
Xavier <1967740181@qq.com>
2022-07-09 16:41:13 +0800

    first commit

--- 失物招领系统（QT作业）/main.cpp
+++ 失物招领系统（QT作业）/main.cpp
@@ -1,10 +1,11 @@
-#include "widget.h"
+﻿#include "widget.h"
 #include "from.h"
 #include "release.h"
 #include "release_2.h"
 #include "release_3.h"
 #include "person_interface.h"
 #include <QApplication>
+
 
 int main(int argc, char *argv[])
 {
